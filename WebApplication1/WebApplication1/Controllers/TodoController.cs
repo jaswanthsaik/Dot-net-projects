22a923aa35487ba9c169dd86f627acfcb8e2d174
Jaswanth sai <119839943+jaswanthsaik@users.noreply.github.com>
2023-09-14 08:07:41 +0530

    project added

---
+++ WebApplication1/WebApplication1/Controllers/TodoController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private static List<TodoItem> _todos = new List<TodoItem>();
+        private static int _nextId = 1; // Keep track of the next available ID
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TodoItem>> Get()
+        {
+            return _todos;
+        }
+
+        [HttpPost]
+        public ActionResult<TodoItem> Post(TodoItem todo)
+        {
+            todo.Id = _nextId; // Assign the next available ID
+            _todos.Add(todo);
+            _nextId++; // Increment the next available ID
+            return CreatedAtAction(nameof(Get), new { id = todo.Id }, todo);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace WebApplication1.models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
